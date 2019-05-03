2c95c7cc832664901a42908e59077cf903845d0d
Shaine Leibowitz <shainedl@gmail.com>
2019-05-03 13:12:04 -0400

    update home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,8 +5,11 @@ export default class HomePage extends Component {
     render() {
       return (
         <div className="home-container">
-            <div className="logo-background">
+            {/* <div className="logo-background">
                 <img className="img-logo" src={`../../../assets/images/logo2.jpg`}/>
+            </div> */}
+            <div className="head-nrc">
+                <p>NEWS REALITY CHECK</p>
             </div>
             <div className="know-ballot">
                 <Link to="/quiz">Get Started!</Link>
@@ -17,12 +20,6 @@ export default class HomePage extends Component {
             <iframe width="560" height="450"
                 src="https://www.youtube.com/embed/8oqJiNzJQ_E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
             </iframe>
-            <div className="know-ballot">
-                <p>Know your ballot.</p>
-            </div>
-            <div className="home-body">
-                <p>Enter the voting booth with more knowledge than ever with LumiVote, a voter education tool. LumiVote helps you learn about the candidates on your ballot through easy-to-use candidate pages and quizzes. Mark the candidates you’ve learned about and plan to vote for, and save, email or print your data to keep track of it.</p>  
-            </div> 
         </div>
       );
     }
